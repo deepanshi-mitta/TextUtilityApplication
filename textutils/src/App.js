05406dce6a96131f290ee8d2bd3ec3db228b8787
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2022-02-10 01:49:45 +0530

    Navbar along with props concept

---
+++ textutils/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './component/Navbar';
+function App() {
+  return (
+    <>
+  <Navbar title="TextUtils" about="AboutTextutils"/>
+    </>
+  );
+}
+
+export default App;
