31572c5a3579efb11d58d3a36c60a83417167417
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2022-02-11 12:15:06 +0530

    added clear text button

--- textutils/src/component/TextForm.js
+++ textutils/src/component/TextForm.js
@@ -8,6 +8,10 @@ export default function TextForm(props) {
  const handleOnChange=(event)=>{
    setText(event.target.value);
  }
+ const handleClearclick=()=>{
+ let newText='';
+ setText(newText);
+ }
  const handleclick=()=>{
    let textnew=text.toLowerCase();
    setText(textnew);
@@ -23,20 +27,23 @@ export default function TextForm(props) {
           id="myBox"
           value={text}
           onChange={handleOnChange}
-          rows="12"
+          rows="8"
         ></textarea>
       </div>
-    <button className="btn btn-primary" onClick={handleupClick}>
+    <button className="btn btn-primary mx-4" onClick={handleupClick}>
         Convert to Uppercase
-      </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </button>
       <button className="btn btn-primary " onClick={handleclick}>
         Convert to LowerCase
+      </button>
+      <button className="btn btn-primary mx-4" onClick={handleClearclick}>
+        Clear Text
       </button>
       </div>
         <div className="cont my-3">
           <h2>Your Text summary</h2>
           <p>{text.split(" ").length} words and {text.length} characters</p>
-          <p >{0.008 *text.split(" ").length}&nbsp;&nbsp;Miniutes to read {text.split(" ").length} word</p>
+          <p>{0.008 *text.split(" ").length}  Miniutes to read {text.split(" ").length} word</p>
           <h2 className="head mt-2">Preview</h2>
           <p>{text}</p>
         </div>
