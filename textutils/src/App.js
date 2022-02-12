503204c428809e68b32b92f868928abfe53bc244
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2022-02-12 23:03:03 +0530

    deploy page on github

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,59 +1,50 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './component/Navbar';
-import TextForm from './component/TextForm';
-import Alert from './component/Alert';
-import About from './component/About';
-import{
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import TextForm from "./component/TextForm";
+import Alert from "./component/Alert";
+
 function App() {
-  const [mode,setmode]=useState('light');
-  const [alert,setalert]=useState(null);
-  const showAlert=(message,type)=>{
+  const [mode, setmode] = useState("light");
+  const [alert, setalert] = useState(null);
+  const showAlert = (message, type) => {
     setalert({
-      msg:message,
-      type:type
-    })
-    setTimeout(()=>{
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
       setalert(null);
-    },1500)
-
-  }
-  const toggleMode=()=>{
-    if(mode==='light'){
-      setmode('dark');
-      document.body.style.backgroundColor='#042743';
-      showAlert("Dark mode has been enabled","succes");
-      document.title='TextUtils-Darkmode';
-
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "succes");
+      document.title = "TextUtils-Darkmode";
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "succes");
+      document.title = "TextUtils-Lightmode";
     }
-    else{
-      setmode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode has been enabled","succes");
-      document.title='TextUtils-Lightmode';
-    }
-  }
+  };
   return (
     <>
-    <Router>
-  <Navbar title="TextUtils" about="AboutTextutils" mode={mode} toggleMode={toggleMode}/>
-  <Alert alert={alert}/>
-  <div className="container my-3">
-    <Switch>
-      <Route exact path="/about">
-        <About/>
-        </Route>
-        <Route exact path="/">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below"mode={mode}/>
-        </Route>
-    </Switch>
-  </div>
-  </Router>
-
+      <Navbar
+        title="TextUtils"
+        about="AboutTextutils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze below"
+          mode={mode}
+        />
+      </div>
     </>
   );
 }
