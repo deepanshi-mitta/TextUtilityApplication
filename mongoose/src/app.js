562e4daef379c39b64d323740e848727617ff823
deepanshi_mitta <deepanshi.mittal_cs19@gla.ac.in>
2022-03-01 17:30:10 +0530

    update ,delete and validations

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -6,8 +6,12 @@ mongose.connect("mongodb://localhost:27017/deepu",{useNewUrlParser:true,useUnifi
 .catch((err=>console.log(err)));
 //define Schema of our database
 const playlistSchema=new mongose.Schema({name:{type:String,
-required:true},
-ctype:String,
+required:true,
+trim:true},
+ctype:{type:String,
+    required:true,
+    lowercase:true,
+    enum:["front end","back End","database"]},
 videos:Number,
 author:String,
 active:Boolean,
@@ -26,38 +30,39 @@ const Playlist=new mongose.model("deepu",playlistSchema);
 const createDocument=async()=>{
     try{
 const jsPlaylist=new Playlist({
-    name:"JavaScript",
-        ctype:"Front End",
+    name:"JavaScript         ",
+        ctype:"front end",
         videos:40,
         author:"Deepanshi",
         active:true,
 })
-const mongoPlaylist=new Playlist({
-    name:"MongoDB",
-        ctype:"back End",
-        videos:70,
-        author:"Deepanshi",
-        active:true,
-})
-const mongoosePlaylist=new Playlist({
-    name:"mongoose",
-        ctype:"back End",
-        videos:90,
-        author:"Deepanshi",
-        active:true,
-})
-const expressPlaylist=new Playlist({
-    name:"Express JS",
-        ctype:"back End",
-        videos:30,
-        author:"Deepanshi",
-        active:true,
-})
+// const mongoPlaylist=new Playlist({
+//     name:"MongoDB",
+//         ctype:"back End",
+//         videos:70,
+//         author:"Deepanshi",
+//         active:true,
+// })
+//const mongoosePlaylist=new Playlist({
+//     name:"mongoose",
+//         ctype:"back End",
+//         videos:90,
+//         author:"Deepanshi",
+//         active:true,
+// })
+// const expressPlaylist=new Playlist({
+//     name:"Express JS",
+//         ctype:"back End",
+//         videos:30,
+//         author:"Deepanshi",
+//         active:true,
+// })
 
 //save our document
 //reactPlaylist.save();
 //save method return a promise.So t takes times to insert so instead of promise we use async await
-const result=await Playlist.insertMany([jsPlaylist,mongoPlaylist,mongoosePlaylist,expressPlaylist]);//Now it will not take time and await cannot work without async function
+// const result=await Playlist.insertMany([jsPlaylist,mongoPlaylist,mongoosePlaylist,expressPlaylist]);//Now it will not take time and await cannot work without async function
+ result=await Playlist.insertMany([jsPlaylist]);
 console.log(result);
 }
 
@@ -65,16 +70,42 @@ catch(err){
     console.log(err);
 }
 }
-//createDocument();
+createDocument();
 
 //How to read a document
 const getDocument=async()=>{
     try{
-    const data= await Playlist.find({ctype:"Front End"}).select({name:1}).limit(2);
+    const data= await Playlist.find({author:"Deepanshi"}).select({name:1}).sort({name:-1});
     console.log(data);
 
 }catch(err){
     console.log(err);
 }
 }
-getDocument();
+//getDocument();
+
+//update document in mongoDB
+const updateDocument=async(_id)=>{
+    try{
+const result=await Playlist.findByIdAndUpdate({_id},{$set:{ctype:"Back End"}},{use:true,useFindAndModify:false});
+console.log(result);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+//updateDocument("6216f4f0f8bd12803ffb02c6");
+
+//delete the documents
+const deleteDocument=async(_id)=>{
+    try{
+const result=await Playlist.findByIdAndDelete({_id});
+console.log(result);
+}
+
+catch(err){
+    console.log(err);
+
+}
+}
+//deleteDocument("621a80eca7e3ac36d9c1b0df");
